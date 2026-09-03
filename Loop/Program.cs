c29d63a1c253cbfdce2b99d4ded60e3d36f41109
sanzid943 <sanzidmostofa@gmail.com>
2026-09-03 19:12:48 +0600

    add c# practice problems

---
+++ Loop/Program.cs
@@ -0,0 +1,11 @@
+﻿// Problem_1
+
+Console.Write("enter a number: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(num + " x " + (i + 1) + " = " + (num * (i + 1)));
+}
+
+Console.WriteLine();
