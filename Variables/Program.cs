31c2a25b7ecf28675128aacba7994fc8b9b50d2b
sanzid943 <sanzidmostofa@gmail.com>
2026-09-03 19:34:43 +0600

    variables problems added

---
+++ Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿// Problem_1
+
+//Console.Write("enter your name: ");
+//string name = Console.ReadLine();
+
+//Console.Write("enter your age: ");
+//int age = int.Parse(Console.ReadLine());
+
+//Console.Write("eneter your city: ");
+//string city = Console.ReadLine();
+
+//Console.WriteLine();
+//Console.WriteLine("name: " + name);
+//Console.WriteLine("age: " + age);
+//Console.WriteLine("city: " + city);
+
+
+
+// Problem_2
+
+Console.Write("enter first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("enter second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+Console.WriteLine();
