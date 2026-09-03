6883d89ca97fa6bfc596f39219d8d4ca82f39ad4
sanzid943 <sanzidmostofa@gmail.com>
2026-09-04 00:42:54 +0600

    add dictionary program

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Problem_1
+
+Dictionary<string, string> studentGrades = new Dictionary<string, string>();
+
+Console.Write("how many students? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+
+    Console.Write("enter student ID: "+(i + 1)+": ");
+    string studentId = Console.ReadLine();
+
+    Console.Write("enter name of student " + (i + 1) + ": ");
+    string name = Console.ReadLine();
+
+
+    studentGrades.Add(studentId, name);
+}
+
+Console.WriteLine("\nStudent list: ");
+
+foreach (var student in studentGrades)
+{
+    Console.WriteLine("Student ID: " + student.Key + ", Name: " + student.Value);
+}
+
+Console.WriteLine();
