ed66a3874a25bb92a7d897e204bc0a54e89cbdb6
sanzid943 <sanzidmostofa@gmail.com>
2026-09-04 11:23:53 +0600

    add encapsulation program

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Example_1
+
+student s1 = new student();
+
+s1.Name = "Rakib";
+s1.Age = 23;
+
+Console.WriteLine("Student name: " + s1.Name);
+Console.WriteLine("Student age: " + s1.Age);
+
+class student
+{
+    private string name;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+}
+
+
+
