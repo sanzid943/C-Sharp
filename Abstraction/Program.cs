523ba999728df6a75f946fb2fc6ef6b78476c98f
sanzid943 <sanzidmostofa@gmail.com>
2026-09-04 11:00:04 +0600

    add abstraction program

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Example_1
+
+Dog dog = new Dog();
+
+dog.makeSound();
+dog.eat();
+
+abstract class Animal
+{
+    public abstract void makeSound();
+    public void eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+}
+
+class Dog : Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+
