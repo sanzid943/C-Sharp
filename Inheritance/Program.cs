260462a0807a0e6f0e35a0f1404e1f2ed920b5f9
sanzid943 <sanzidmostofa@gmail.com>
2026-09-04 10:51:05 +0600

    add inheritance program

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Example_1
+
+Dog dog = new Dog();
+
+dog.eat();
+dog.bark();
+
+class Animal
+{
+    public void eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+}
+
+class Dog : Animal
+{
+    public void bark()
+    {
+        Console.WriteLine("Dog is barking");
+    }
+}
+
