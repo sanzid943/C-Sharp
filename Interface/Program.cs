c072d13c82234555c987dd11e4389036ef8f0f7e
sanzid943 <sanzidmostofa@gmail.com>
2026-09-04 10:43:24 +0600

    add interface programs

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Example_1
+
+Dog dog = new Dog();
+dog.makeSound();
+
+interface IAnimal
+{
+    void makeSound();
+}
+
+class Dog : IAnimal
+{
+    public void makeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+
+
+
+
+// Example_2
+
+IPayment payment = new Bkash();
+payment.pay();
+
+IPayment payment2 = new Card();
+payment2.pay();
+
+interface IPayment
+{
+    void pay();
+}
+
+class Bkash : IPayment
+{
+    public void pay()
+    {
+        Console.WriteLine("Payment done via Bkash");
+    }
+}
+
+class Card : IPayment
+{
+    public void pay()
+    {
+        Console.WriteLine("Payment done via Card");
+    }
+}
+
