87ae7e8101adc3ae0b0f096e5360bd757a4e66f2
sanzid943 <sanzidmostofa@gmail.com>
2026-09-07 16:01:10 +0600

    add list programs

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,28 +1,74 @@
 ﻿// Problem_1
 
-Dictionary<string, string> studentGrades = new Dictionary<string, string>();
+//Dictionary<string, string> studentGrades = new Dictionary<string, string>();
 
-Console.Write("how many students? ");
-int n = int.Parse(Console.ReadLine());
+//Console.Write("how many students? ");
+//int n = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < n; i++)
+//for (int i = 0; i < n; i++)
+//{
+
+//    Console.Write("enter student ID: "+(i + 1)+": ");
+//    string studentId = Console.ReadLine();
+
+//    Console.Write("enter name of student " + (i + 1) + ": ");
+//    string name = Console.ReadLine();
+
+
+//    studentGrades.Add(studentId, name);
+//}
+
+//Console.WriteLine("\nStudent list: ");
+
+//foreach (var student in studentGrades)
+//{
+//    Console.WriteLine("Student ID: " + student.Key + ", Name: " + student.Value);
+//}
+
+//Console.WriteLine();
+
+
+
+
+// Problem_2
+
+//Dictionary<int, string> students= new Dictionary<int, string>();
+
+//students.Add(11, "karim");
+//students.Add(12, "rafi");
+//students.Add(13, "raju");
+//students.Add(14, "kalam");
+
+//foreach (var student in students)
+//{
+//    Console.WriteLine(student);
+//}
+
+//Console.WriteLine();
+
+//students.Remove(14);
+
+//foreach(var student in students)
+//{
+//    Console.WriteLine("ID: " + student.Key);
+//    Console.WriteLine("Name: " + student.Value);
+//}
+
+
+
+
+
+// Problem_3
+
+Dictionary<int, int> students= new Dictionary<int, int>();
+
+students.Add(101, 67);
+students.Add(102, 78);
+students.Add(103, 87);
+students.Add(104, 90);
+
+foreach (var student in students)
 {
-
-    Console.Write("enter student ID: "+(i + 1)+": ");
-    string studentId = Console.ReadLine();
-
-    Console.Write("enter name of student " + (i + 1) + ": ");
-    string name = Console.ReadLine();
-
-
-    studentGrades.Add(studentId, name);
+    Console.WriteLine("ID: " + student.Key);
+    Console.WriteLine("Marks: " + student.Value);
 }
-
-Console.WriteLine("\nStudent list: ");
-
-foreach (var student in studentGrades)
-{
-    Console.WriteLine("Student ID: " + student.Key + ", Name: " + student.Value);
-}
-
-Console.WriteLine();
--- List/Program.cs
+++ List/Program.cs
@@ -41,7 +41,7 @@ foreach (string name in names)
 
 // Problem_3
 
-List<int> numbers= new List<int>();
+List<int> numbers = new List<int>();
 
 numbers.Add(231);
 numbers.Add(212);
@@ -53,3 +53,126 @@ foreach (int number in numbers)
     Console.WriteLine(number);
 }
 
+
+
+
+// Problem_4
+
+List<int> numbers = new List<int>();
+
+numbers.Add(123);
+numbers.Add(232);
+numbers.Add(245);
+numbers.Add(157);
+
+int sum = 0;
+
+foreach (int i in numbers)
+{
+    sum += i;
+}
+
+Console.WriteLine("Sum: " + sum);
+
+
+
+
+// Problem_5
+
+List<int> numbers = new List<int>()
+{ 10, 20, 23, 35, 57, 67, 35, 89, 896};
+
+foreach (var number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number + " is even");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine(number + " is odd");
+        Console.WriteLine();
+    }
+}
+
+
+
+
+// Problem_6
+
+List<string> names = new List<string>()
+{ "rahim", "karim", "tanvir", "raju", "rafiq"};
+
+Console.WriteLine("enter name: ");
+string name = Console.ReadLine();
+
+if (names.Contains(name))
+{
+    Console.WriteLine("name found");
+}
+
+else
+{
+    Console.WriteLine("name not found");
+}
+Console.WriteLine();
+
+
+
+
+// Problem_7
+
+List<int> numbers = new List<int>()
+{ 23, 12, 56, 46, 68, 25, 78};
+
+foreach (var number in numbers)
+{
+    Console.Write(number + "   ");
+}
+
+Console.WriteLine();
+
+
+numbers.Reverse();
+Console.WriteLine();
+
+foreach (var number in numbers)
+{
+    Console.Write(number + "   ");
+
+}
+
+numbers.Sort();
+Console.WriteLine();
+
+foreach (var number in numbers)
+{
+    Console.Write(number + "   ");
+}
+
+
+
+
+// problem_8
+
+List<int> numbers= new List<int>()
+{ 23, 56, 24, 17, 89, 67, 54, 45 };
+
+int largest=0;
+
+foreach(int num in numbers)
+{
+    Console.Write(num + "  ");
+}
+Console.WriteLine();
+
+foreach(int number in numbers)
+{
+    if (number > largest)
+    {
+        largest=number;
+    }
+}
+
+Console.WriteLine("Largest number is: " + largest);
