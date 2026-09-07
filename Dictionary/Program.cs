87af1dee14cc80ba43775d1fe1e637729f88c73c
sanzid943 <sanzidmostofa@gmail.com>
2026-09-07 16:18:44 +0600

    add dictionary programs

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,58 +1,58 @@
 ﻿// Problem_1
 
-//Dictionary<string, string> studentGrades = new Dictionary<string, string>();
+Dictionary<string, string> studentGrades = new Dictionary<string, string>();
 
-//Console.Write("how many students? ");
-//int n = int.Parse(Console.ReadLine());
+Console.Write("how many students? ");
+int n = int.Parse(Console.ReadLine());
 
-//for (int i = 0; i < n; i++)
-//{
+for (int i = 0; i < n; i++)
+{
 
-//    Console.Write("enter student ID: "+(i + 1)+": ");
-//    string studentId = Console.ReadLine();
+    Console.Write("enter student ID: " + (i + 1) + ": ");
+    string studentId = Console.ReadLine();
 
-//    Console.Write("enter name of student " + (i + 1) + ": ");
-//    string name = Console.ReadLine();
+    Console.Write("enter name of student " + (i + 1) + ": ");
+    string name = Console.ReadLine();
 
 
-//    studentGrades.Add(studentId, name);
-//}
+    studentGrades.Add(studentId, name);
+}
 
-//Console.WriteLine("\nStudent list: ");
+Console.WriteLine("\nStudent list: ");
 
-//foreach (var student in studentGrades)
-//{
-//    Console.WriteLine("Student ID: " + student.Key + ", Name: " + student.Value);
-//}
+foreach (var student in studentGrades)
+{
+    Console.WriteLine("Student ID: " + student.Key + ", Name: " + student.Value);
+}
 
-//Console.WriteLine();
+Console.WriteLine();
 
 
 
 
 // Problem_2
 
-//Dictionary<int, string> students= new Dictionary<int, string>();
+Dictionary<int, string> students = new Dictionary<int, string>();
 
-//students.Add(11, "karim");
-//students.Add(12, "rafi");
-//students.Add(13, "raju");
-//students.Add(14, "kalam");
+students.Add(11, "karim");
+students.Add(12, "rafi");
+students.Add(13, "raju");
+students.Add(14, "kalam");
 
-//foreach (var student in students)
-//{
-//    Console.WriteLine(student);
-//}
+foreach (var student in students)
+{
+    Console.WriteLine(student);
+}
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//students.Remove(14);
+students.Remove(14);
 
-//foreach(var student in students)
-//{
-//    Console.WriteLine("ID: " + student.Key);
-//    Console.WriteLine("Name: " + student.Value);
-//}
+foreach (var student in students)
+{
+    Console.WriteLine("ID: " + student.Key);
+    Console.WriteLine("Name: " + student.Value);
+}
 
 
 
@@ -60,15 +60,58 @@
 
 // Problem_3
 
-Dictionary<int, int> students= new Dictionary<int, int>();
+using System.Globalization;
 
-students.Add(101, 67);
-students.Add(102, 78);
-students.Add(103, 87);
-students.Add(104, 90);
+Dictionary<int, int> marks = new Dictionary<int, int>();
 
-foreach (var student in students)
+marks.Add(101, 67);
+marks.Add(102, 78);
+marks.Add(103, 87);
+marks.Add(104, 90);
+
+foreach (var student in marks)
 {
     Console.WriteLine("ID: " + student.Key);
     Console.WriteLine("Marks: " + student.Value);
+}
+
+Console.WriteLine();
+Console.WriteLine("enter ID: ");
+int id = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (marks.ContainsKey(id))
+{
+    Console.WriteLine("your number is: " + marks[id]);
+}
+else
+{
+    Console.WriteLine("student not found");
+}
+
+
+
+
+
+// Problem_4
+
+Dictionary<int, string> names= new Dictionary<int, string>();
+
+names.Add(101, "kalam");
+names.Add(102, "rajib");
+names.Add(103, "rakib");
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+names[102] = "rafi";
+
+Console.WriteLine();
+Console.WriteLine("update value: ");
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
 }
