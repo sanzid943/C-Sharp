c9d33705c08251a1b188af44ecec61716f0be54c
sanzid943 <sanzidmostofa@gmail.com>
2026-09-07 13:31:50 +0600

    add list programs

--- List/Program.cs
+++ List/Program.cs
@@ -2,6 +2,23 @@
 
 List<string> names = new List<string>();
 
+names.Add("rahim");
+names.Add("karim");
+names.Add("rafi");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+
+
+
+
+// Problem_2
+
+List<string> names = new List<string>();
+
 Console.Write("how many students? ");
 int n = int.Parse(Console.ReadLine());
 
@@ -17,3 +34,22 @@ foreach (string name in names)
 {
     Console.WriteLine(name);
 }
+
+
+
+
+
+// Problem_3
+
+List<int> numbers= new List<int>();
+
+numbers.Add(231);
+numbers.Add(212);
+numbers.Add(123);
+numbers.Add(987);
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
