84a8484dbff9b1b8c0714eace1f003b3a2facfc3
sanzid943 <sanzidmostofa@gmail.com>
2026-09-08 23:43:58 +0600

    add generic method programs

---
+++ Generic Method/Program.cs
@@ -0,0 +1,60 @@
+﻿// Problem-1
+
+//class Box<T>
+//{
+//    private T Value;
+
+//    public void setValue(T value)
+//    {
+//        this.Value = value;
+//    }
+//    public T getValue()
+//    {
+//        return Value;
+//    }
+//}
+
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Box<int> box = new Box<int>();
+//        box.setValue(42);
+
+//        Console.WriteLine("ID: " + box.getValue());
+
+//        Box<string> box1 = new Box<string>();
+//        box1.setValue("zid");
+
+//        Console.WriteLine("name: " + box1.getValue());
+//    }
+//}
+
+
+
+// Problem-2
+
+//class Program
+//{
+//    static void print<T>(T value)
+//    {
+//        Console.WriteLine(value);
+//    }
+
+//    static void Main()
+//    {
+//        print<int>(120);
+//        print<string>("sakib");
+//        print<double>(3.67);
+//        print<char>('A');
+//        print<bool>(true);
+
+//    }
+//}
+
+
+
+
+// Problem-3
+
